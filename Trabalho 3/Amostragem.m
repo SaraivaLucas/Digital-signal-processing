5d3eebedfa30f17f4016b9b636910369d661f633
Lucas saraiva <Lucas_saraiva@outlook.com.br>
2017-10-18 22:56:30 -0300

    ATT

--- Trabalho 3/Amostragem.m
+++ Trabalho 3/Amostragem.m
@@ -1,7 +1,7 @@
 clc;clear all;close all;
 %% Parâmetros
 t_0=5e-3;                                                   % Tempo de "Período"
-N_ele=2048;                                                 % Número de elementos do Sinal
+N_ele=256;                                                  % Número de elementos do Sinal
 T=t_0/N_ele;                                                % Taxa de amostragem (Passo entre as amostras)
 fs=1/T;                                                     % Frequência de amostragem
 t=-t_0:1/fs:t_0-1/fs;                                       % Eixo do tempo
@@ -22,7 +22,7 @@ Xa=dtft(xv,t,freq);                                         % Espectro do sinal 
 subplot(3,2,2)
 plot(freq,abs(Xa))
 title('Sinal Não Amostrado na Frequência')
-axis([-50000 50000 0 1.2*max(abs(Xa))])
+axis([-fs/2 fs/2 0 1.2*max(abs(Xa))])
 
 %% Amostragem 1kHz
 fs1k=1e3;                                                   % Nova frequência de amostragem 1kHz
@@ -31,7 +31,7 @@ x_samp1=downsample(xv,N);                                   % Coleta 1 amostra a
 x_samp1=upsample(x_samp1,N);                                % Retorna vetor amostrado com o número inicial de elementos
 x_samp1=x_samp1(1,1:length(t));                             % Corrige comprimento de vetores
 % Plotting
-subplot(3,2,5)
+subplot(3,2,3)
 plot(t,x_samp1(1,1:length(t)))                 
 title('Sinal Amostrado no Tempo')
 axis([-t_0 t_0 0 1.2*max(abs(x_samp1))])
@@ -39,10 +39,10 @@ axis([-t_0 t_0 0 1.2*max(abs(x_samp1))])
 %% Aplicando a transformada de Fourier ao sinal Amostrado 1kHz
 Xa1=dtft(x_samp1,t,freq);                                   % Espectro do sinal DTFT
 % Plotting
-subplot(3,2,6)
+subplot(3,2,4)
 plot(freq,abs(Xa1))
 title('Sinal Amostrado na Frequência')
-axis([-50000 50000 0 1.2*max(abs(Xa1))])
+axis([-fs/2 fs/2  0 1.2*max(abs(Xa1))])
 
 %% Amostragem 5kHz
 fs5k=5e3;                                                   % Nova frequência de amostragem 5kHz
@@ -51,7 +51,7 @@ x_samp5=downsample(xv,N);                                   % Coleta 1 amostra a
 x_samp5=upsample(x_samp5,N);                                % Retorna vetor amostrado com o número inicial de elementos
 x_samp5=x_samp5(1,1:length(t));                             % Corrige comprimento de vetores
 % Plotting
-subplot(3,2,3)
+subplot(3,2,5)
 plot(t,x_samp5(1,1:length(t)))                 
 title('Sinal Amostrado no Tempo')
 axis([-t_0 t_0 0 1.2*max(abs(x_samp5))])
@@ -59,7 +59,8 @@ axis([-t_0 t_0 0 1.2*max(abs(x_samp5))])
 %% Aplicando a transformada de Fourier ao sinal Amostrado 5kHz
 Xa5=dtft(x_samp5,t,freq);                                   % Espectro do sinal DTFT   
 % Plotting
-subplot(3,2,4)
+subplot(3,2,6)
 plot(freq,abs(Xa5))
 title('Sinal Amostrado na Frequência')
-axis([-50000 50000 0 1.2*max(abs(Xa5))])
+axis([-fs/2 fs/2  0 1.2*max(abs(Xa5))])
+save('Amostragem5k')
--- Trabalho 3/Reconstrucao.m
+++ Trabalho 3/Reconstrucao.m
@@ -1,43 +1,45 @@
 % Reconstrucao_sinc.m
-close all; clear all; clc;
+close all;clear all; clc;
 %% Parâmetros
 % Coletaremos todos os dados do processo de amostragem para que o código
 % não fique extenso. Todas as variáveis terão o mesmo nome. O arquivo .mat
 % deve sempre está na pasta em que o script está.
-load('handsOn7_matlab_codes/Amostragem.mat')
-
+load('/Amostragem5k.mat')
+f_0=fs/N_ele;                                                    % Passo de frequencia.: f_0=freq(2)-freq(1)
+close all
+Tf=(t_0-1/fs)
 %% Reconstrução via Sincs
-nSamples = length(s_out);                                        % Mede-se o comprimento do sinal
-xSamples = 0 : nSamples-1;                                       % Vetor ordenado de amostras
-trec = nSamples/Tf*[0:T:Tf];                                     % Eixo do tempo do sinal recuperado
-Tsinc = 0.002;                                                   % Passo de tempo da sinc
-Tfsinc = 50;                                                     % Tempo Final da sinc
-tsinc = 0:Tsinc:Tfsinc;                                          % Eixo de tempo da sinc
-s_recv=0;                                                        % Cria o vetor da reconstrução
-Bs = fm1;                                                        % Banda da transmissão
-
-% Gera laço para somatório
-for ik = xSamples      
-Nx_sinc = s_out(ik+1)*sinc(2*pi*Bs*(tsinc-ik*T));                % Cria sinc para a amostra ik
-s_recv = s_recv+Nx_sinc;                                         % Faz somatórios das sincs
-end
-s_recv=s_recv(1:lfft);                                           % Corrige comprimento do vetor
-s_recv=s_recv*(max(m_t)/max(s_recv));                            % Ajusta o ganho
-
-%% Plotting
-% Plota o sinal reconstruído por um Sincs no domínio do tempo
-% Plota sinal recuperado na frequência
-S_recv=fftshift(fft(s_recv,lfft)/lfft);
-subplot(2, 1, 1);
-sfig2a=plot(freq,abs(S_recv));
-xlabel('Frequência (Hz)');
-axis([-150 150 0 .6]);
-title('Espectro do sinal reconstruido');
-
-% Plota sinal recuperado no tempo
-subplot(2, 1, 2);
-sfig2b=plot(t,m_t,'k-.',t,s_recv(1:lfft),'b');
-legend('Sinal original','Sinal reconstruido');
-xlabel('Tempo (sec)');
-title('Sinal Original x Sinal Reconstruido');
-set(sfig2b,'Linewidth',2);
+ yrec = x_samp5;
+        TsNew = T;
+        x_recon=0;
+        nSamples = length(yrec);
+        xSamples = 0 : nSamples-1;
+        trec = nSamples/Tf*[0:T:Tf];
+        trec2=trec;
+        tSinc = 0.005;
+        Tfsinc = t_0-1/fs;
+        for ik = xSamples
+            %lsin=ik:-tSinc:-nSamples+ik;
+            tnew = -t_0:T/N_ele:Tfsinc;
+            x_recon=x_recon+yrec(ik+1)*sinc(2*pi*fs*(tnew-ik*1/fs));
+ 
+        end
+        
+            figure(2);
+            subplot(2,1,1);
+            stem(t, x_samp5,'filled');
+            hold on;
+            plot(t,xv,'r-o');
+            title('Sinal de "Tempo Cont�nuo"')
+            xlabel('t');
+            ylabel('x(t)');
+            plot(tnew,yrec(ik+1)*sinc(2*pi*fs*(tnew-ik*1/fs)),'k')
+            axis([-Tf Tf -2 2]);
+            
+            subplot(2,1,2);
+            plot(t,xv,'r');
+            title('Sinal de "Tempo Cont�nuo"')
+            xlabel('t');
+            ylabel('x(t)');
+            hold on;
+            plot(tnew,x_recon,'k');
